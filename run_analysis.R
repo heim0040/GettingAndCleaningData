192a5307d5ed9d1389d12e29e943dfef342447a8
heim0040 <heim0040@hotmail.com>
2016-05-20 21:49:16 -0700

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+# download data
+library(downloader)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download(fileUrl, dest="dataset.zip", mode="wb")
+unzip("dataset.zip", exdir = "./")
+
+# prepare test data
+testdat <- read.table("UCI HAR Dataset/test/X_test.txt", header=FALSE)
+testlabels <- read.table("UCI HAR Dataset/test/Y_test.txt", header=FALSE)
+testsubjects <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE)
+features <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)
+colnames(testdat) <- features[,2]
+testdata <- testdat[ , grep( "Mean|mean|std", colnames(testdat))]
+test <- cbind(testlabels, testdata)
+test2 <- cbind(testsubjects, test)
+colnames(test2)[1:2] <- c("Subject", "ActivityNumber")
+
+# prepare training data
+traindat <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE)
+trainlabels <- read.table("UCI HAR Dataset/train/Y_train.txt", header=FALSE)
+trainsubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
+colnames(traindat) <- features[,2]
+traindata <- traindat[ , grep( "Mean|mean|std", colnames(traindat))]
+train <- cbind(trainlabels, traindata)
+train2 <- cbind(trainsubjects, train)
+colnames(train2)[1:2] <- c("Subject", "ActivityNumber")
+
+# combine test and train datasets
+merged <- rbind(test2, train2)
+merged$Activity <- activities$V2[match(merged$ActivityNumber,activities$V1)]
+
+# reorder columns
+col_idx <- grep("Activity", names(merged))
+merged <- merged[,c(col_idx, (1:ncol(merged))[-col_idx])]
+
+library(dplyr)
+ActivityData <-
+        merged %>%
+        group_by(Activity,Subject) %>%
+        summarise_each(funs(mean))
+
+
+
