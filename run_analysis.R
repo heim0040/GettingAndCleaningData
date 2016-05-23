3daaa04d4758232e33e9bd7382b226df9a79af96
heim0040 <heim0040@hotmail.com>
2016-05-22 23:00:10 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -11,7 +11,7 @@ testsubjects <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE
 features <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", header=FALSE)
 colnames(testdat) <- features[,2]
-testdata <- testdat[ , grep( "Mean|mean|std", colnames(testdat))]
+testdata <- testdat[ , grep( "mean()|std()", colnames(testdat))]
 test <- cbind(testlabels, testdata)
 test2 <- cbind(testsubjects, test)
 colnames(test2)[1:2] <- c("Subject", "ActivityNumber")
@@ -21,7 +21,7 @@ traindat <- read.table("UCI HAR Dataset/train/X_train.txt", header=FALSE)
 trainlabels <- read.table("UCI HAR Dataset/train/Y_train.txt", header=FALSE)
 trainsubjects <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
 colnames(traindat) <- features[,2]
-traindata <- traindat[ , grep( "Mean|mean|std", colnames(traindat))]
+traindata <- traindat[ , grep( "mean()|std()", colnames(traindat))]
 train <- cbind(trainlabels, traindata)
 train2 <- cbind(trainsubjects, train)
 colnames(train2)[1:2] <- c("Subject", "ActivityNumber")
@@ -33,6 +33,8 @@ merged$Activity <- activities$V2[match(merged$ActivityNumber,activities$V1)]
 # reorder columns
 col_idx <- grep("Activity", names(merged))
 merged <- merged[,c(col_idx, (1:ncol(merged))[-col_idx])]
+merged$ActivityNumber <- NULL
+colnames(merged)[3:81] <- paste("Average.of.", colnames(merged)[3:81], sep="")
 
 library(dplyr)
 ActivityData <-
